44ac31d56cf1cae9c35427d63ab625738782e8ec
Douglas Chen <dchenishimoto@gmail.com>
2023-10-17 22:46:25 -0300

    simple thread implementation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use std::thread;
-use std::time::Duration;
+use std::sync::mpsc;
+
+use rand::thread_rng;
+use rand::Rng;
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
+    // Create MSPC channel
+    let (tx, rx) = mpsc::channel();
+
+    // Thread to generate and send random numbers
+    let handle = thread::spawn(move || {
+        for _ in 1..10 {
+            let num = thread_rng().gen_range(0..100);
+            tx.send(num).unwrap();
         }
+
+        drop(tx); //close channel
     });
 
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
+    // Buffer
+    let mut buffer: Vec<i32> = vec![];
+
+    // Push numbers received from thread into the buffer
+    for num in &rx {
+        buffer.push(num);
     }
+
+    println!{"{:?}", buffer};
+
+    handle.join().unwrap();
 }
