f3218314e4022f31b627457410cf662bced5cfe3
Douglas Chen <dchenishimoto@gmail.com>
2023-10-19 00:41:19 -0300

    adapted to generic type

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,81 @@
-use std::thread;
-use std::sync::mpsc;
-
-use rand::{thread_rng, Rng};
+use std::{
+    thread, 
+    sync::{mpsc, mpsc::TryRecvError::{Empty, Disconnected}}, 
+    time::{Duration, Instant},
+    fmt::Debug
+};
+use rand::{thread_rng, Rng, distributions::Alphanumeric};
 
 fn main() {
     // Create MSPC channel
     let (tx, rx) = mpsc::channel();
 
-    // Inverter produtor e consumidor entre as threads
-    // adicionar delay, mas adicionar um delay maior (simular disco) para o consumidor consumir o buffer de uma vez
-    // gerar como coisas como tipo generico <T>
-
-    // Thread to generate and send random numbers
-    let handle = thread::spawn(move || {
-        for _ in 1..10 {
-            let num = thread_rng().gen_range(0..100);
-            tx.send(num).unwrap();
-        }
-    });
-
-    // Buffer
-    let mut buffer: Vec<i32> = vec![];
-
-    // Push numbers received from thread into the buffer
-    for num in &rx {
-        buffer.push(num);
+    // Start consumer thread
+    thread::spawn(move || { consumer_handle(rx); });
+    
+    // Loop to generate and send data to consumer
+    loop {
+        tx.send(generate_data()).unwrap();
+        thread::sleep(Duration::from_secs(1)); //1s delay
     }
+}
 
-    println!{"{:?}", buffer};
+fn generate_data() -> String {
+    // let data = thread_rng().gen_range(0..100);
+    let data = thread_rng()
+    .sample_iter(&Alphanumeric)
+    .take(4)
+    .map(char::from)
+    .collect();
 
-    handle.join().unwrap();
+    println!("Generated [{:?}] in main thread.", data);
+    data
+}
+
+fn consumer_handle<T: Debug>(rx: mpsc::Receiver<T>) {
+    // Buffer and "fake" disk
+    let mut buffer: Vec<T> = vec![];
+    let mut disk: Vec<T> = vec![];
+
+    // Time control
+    let mut now = Instant::now();
+    const DISK_DELAY: u128 = 5 * u128::pow(10, 6); //5s delay
+
+    // Loop to receive and consume data
+    loop {
+        // Non-blocking receiver
+        match rx.try_recv() {
+            Ok(data) => {
+                // Received data in this iteration!
+                buffer.push(data);
+            }
+            Err(error) => match error {
+                // Did not receive any data in this iteration!
+                Empty => {},
+
+                // Channel disconnected!
+                Disconnected => {
+                    println!("# ERROR: Consumer thread died!");
+                    break
+                }
+            }
+        }
+
+        // Is the disk available now? If so, does my buffer has enough elements?
+        let elapsed_time = now.elapsed().as_micros();
+        if elapsed_time >= DISK_DELAY && buffer.len() >= get_buffer_size() {
+            flush(&mut buffer, &mut disk);
+            now = Instant::now(); //refresh clock
+        }
+    }
+}
+
+// Allows dynamic buffer size
+fn get_buffer_size() -> usize { 1 }
+
+// Write buffer to disk
+fn flush<T: Debug>(buffer: &mut Vec<T>, disk: &mut Vec<T>) {
+    println!{">> Flushed {:?} in consumer thread.", &buffer};
+    disk.append(buffer);
+    println!{">> Now disk has {:?}", disk};
 }
