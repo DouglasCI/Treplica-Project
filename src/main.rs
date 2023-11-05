9efb6e041a3c9bcf3105dbd5c3d3d86f1761bd3f
Douglas Chen <dchenishimoto@gmail.com>
2023-11-04 22:28:03 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,78 @@
+#![allow(dead_code, unused_assignments)] 
+
 use std::{
     thread, 
     sync::{mpsc, mpsc::TryRecvError::{Empty, Disconnected}}, 
     time::{Duration, Instant},
-    fmt::Debug
+    fmt::Debug, 
 };
 use rand::{thread_rng, Rng, distributions::Alphanumeric};
 
-fn main() {
-    // Create MSPC channel
-    let (tx, rx) = mpsc::channel();
-
-    // Start consumer thread
-    thread::spawn(move || { consumer(rx); });
-    
-    // Loop to generate and send data to consumer
-    loop {
-        tx.send(generate_data()).unwrap();
-        thread::sleep(Duration::from_secs(1)); //1s delay
-    }
-
-    // adicionar struct { escrita:T, msg: U }
-    // quebrar buffer (struct) para mandar uma parte pro disco e outra para rede
+#[derive(Clone, Debug)]
+struct Data<T, U> {
+    write: T,
+    message: U
 }
 
-fn generate_data() -> String {
-    // let data = thread_rng().gen_range(0..100);
-    let data = thread_rng()
+const ONE_THOUSAND: u128 = u128::pow(10, 3);
+
+const PRODUCER_DELAY: u64 = ONE_THOUSAND as u64;
+
+const DISK_DELAY: u128 = 6 * ONE_THOUSAND;
+const PATIENCE: u128 = DISK_DELAY + 5 * ONE_THOUSAND;
+const BUFFER_SIZE: usize = 5;
+
+const NETWORK_DELAY: u128 = ONE_THOUSAND;
+const MSGS_PER_INTERVAL: usize = 2;
+
+fn main() {
+    /* Create MSPC channels */
+    // Producer -> Disk
+    let (tx_prod, rx_disk) = mpsc::channel();
+    // Disk -> Network
+    let (tx_disk, rx_net) = mpsc::channel();
+
+    /* Start consumer threads */
+    // Disk
+    thread::spawn(move || { consumer_disk(rx_disk, tx_disk); });
+    // Network
+    thread::spawn(move || { consumer_network(rx_net); });
+    
+    // Loop to generate and send data to consumer
+    let mut counter: u32 = 0; //for message field in Data struct
+    loop {
+        tx_prod.send(producer(counter)).unwrap();
+        counter += 1;
+        thread::sleep(Duration::from_millis(PRODUCER_DELAY));
+    }
+}
+
+fn producer(counter: u32) -> Data<String, u32> {
+    let w: String = thread_rng()
     .sample_iter(&Alphanumeric)
     .take(4)
     .map(char::from)
     .collect();
 
-    println!("Generated [{:?}] in main thread.", data);
+    let data = Data{ write: w, message: counter };
+
+    println!("@[PRODUCER]>> Generated [{:?}].", data);
     data
 }
 
-fn consumer<T: Debug>(rx: mpsc::Receiver<T>) {
-    // Buffer and "fake" disk
-    let mut buffer: Vec<T> = vec![];
-    let mut disk: Vec<T> = vec![];
+fn consumer_disk<T: Clone + Debug, U: Clone + Debug>
+                (rx_disk: mpsc::Receiver<Data<T, U>>, tx_disk: mpsc::Sender<Vec<U>>) {
+    // Structures
+    let mut buffer: Vec<Data<T, U>> = vec![]; //data buffer
+    let mut disk: Vec<T> = vec![]; //false disk
+    let mut not_ready_msg_buffer: Vec<U> = vec![]; //message buffer to be sent to network
 
     // Time control
-    let mut now = Instant::now();
+    let mut clock = Instant::now();
 
-    // Loop to receive and consume data
     loop {
         // Non-blocking receiver
-        match rx.try_recv() {
+        match rx_disk.try_recv() {
             Ok(data) => {
                 // Received data in this iteration!
                 buffer.push(data);
@@ -54,45 +80,103 @@ fn consumer<T: Debug>(rx: mpsc::Receiver<T>) {
             Err(error) => match error {
                 // Did not receive any data in this iteration!
                 Empty => {},
-
                 // Channel disconnected!
                 Disconnected => {
-                    println!("# ERROR: Consumer thread died!");
-                    break
+                    println!("#[ERROR]>> Consumer disk thread died!");
                 }
             }
         }
 
-        if is_disk_ready(now, buffer.len()) {
-            flush(&mut buffer, &mut disk);
-            now = Instant::now(); //refresh clock
+        let elapsed_time: u128 = clock.elapsed().as_millis();
+        // Is the disk available now? 
+        if elapsed_time >= DISK_DELAY {
+            let mut write_buffer: Vec<T> = vec![];
+            let mut ready_msg_buffer: Vec<U> = vec![];
+            
+            // Since the disk is avaiable now, the not_ready_msg_buffer is ready
+            ready_msg_buffer.append(&mut not_ready_msg_buffer);
+
+            // So start sending the ready_msg_buffer through the network
+            if !ready_msg_buffer.is_empty() {
+                tx_disk.send(ready_msg_buffer).unwrap();
+            }
+            
+            // Is the buffer full? Or is the disk idle for too long and the buffer is not empty?
+            if buffer.len() >= get_buffer_size() || (elapsed_time >= PATIENCE && !buffer.is_empty()) {
+                (write_buffer, not_ready_msg_buffer) = split_data_buffer(&buffer);
+                flush_to_disk(&mut write_buffer, &mut disk);
+                buffer.clear();
+                clock = Instant::now(); //refresh clock
+            }
         }
     }
 }
 
-// Check if it's time to write the buffer to the disk
-// overload heuristica disco e rede
-fn is_disk_ready(now: Instant, buffer_len: usize) -> bool {
-    let elapsed_time: u128 = now.elapsed().as_millis();
-    const ONE_THOUSAND: u128 = u128::pow(10, 3);
-    const DISK_DELAY: u128 = 5 * ONE_THOUSAND; //5000ms delay
-    const PATIENCE: u128 = DISK_DELAY + ONE_THOUSAND; //+1000ms delay
+// Allows dynamic buffer size
+fn get_buffer_size() -> usize { BUFFER_SIZE }
 
-    // Firstly, is the disk available now? 
-    elapsed_time >= DISK_DELAY && 
-    // If so, is the disk idle for too long and the buffer not empty?
-    ((elapsed_time >= PATIENCE && buffer_len > 0) ||
-    // Or is the buffer full?
-    buffer_len >= get_buffer_size())
+// Split data buffer into write and message buffers
+fn split_data_buffer<T: Clone + Debug, U: Clone + Debug>(buffer: &Vec<Data<T, U>>) -> (Vec<T>, Vec<U>) {
+    let mut write_buffer: Vec<T> = vec![];
+    let mut msg_buffer: Vec<U> = vec![];
+
+    for d in buffer {
+        let dd = d.clone();
+        write_buffer.push(dd.write);
+        msg_buffer.push(dd.message);
+    }
+
+    (write_buffer, msg_buffer)
 }
 
-// Allows dynamic buffer size
-fn get_buffer_size() -> usize { 7 }
-
 // Write buffer to disk
-fn flush<T: Debug>(buffer: &mut Vec<T>, disk: &mut Vec<T>) {
-    println!{">> Flushed {:?} in consumer thread.", &buffer};
-    disk.append(buffer);
-    // quebra
-    println!{">> Now disk contains {:?}", disk};
+fn flush_to_disk<T: Debug>(write_buffer: &mut Vec<T>, disk: &mut Vec<T>) {
+    println!{"@[DISK]>> Disk contains {:?}.", disk};
+    println!{"@[DISK]>> Disk is now writing {:?}.", &write_buffer};
+    disk.append(write_buffer);
+}
+
+fn consumer_network<U: Clone + Debug>(rx_net: mpsc::Receiver<Vec<U>>) {
+    // Structures
+    let mut msg_buffer: Vec<U> = vec![]; //buffer for all messages
+    let mut network: Vec<U> = vec![]; //sent messages history
+
+    // Time control
+    let mut clock = Instant::now();
+
+    loop {
+        // Non-blocking receiver
+        match rx_net.try_recv() {
+            Ok(messages) => {
+                // Received data in this iteration!
+                msg_buffer.extend(messages);
+            }
+            Err(error) => match error {
+                // Did not receive any data in this iteration!
+                Empty => {},
+                // Channel disconnected!
+                Disconnected => {
+                    println!("#[ERROR]>> Consumer network thread died!");
+                }
+            }
+        }
+        
+        // Send small slices of the msg_buffer
+        let elapsed_time: u128 = clock.elapsed().as_millis();
+        let msg_qty = get_msgs_per_interval();
+        if elapsed_time >= NETWORK_DELAY && msg_buffer.len() >= msg_qty {
+            let mut v = msg_buffer.drain(..msg_qty).collect();
+            send_to_network(&mut v, &mut network);
+            clock = Instant::now(); //refresh clock
+        }
+    }
+}
+
+// Allows dynamic amount of messages sent to network per interval
+fn get_msgs_per_interval() -> usize { MSGS_PER_INTERVAL }
+
+fn send_to_network<U: Debug>(msg_buffer: &mut Vec<U>, network: &mut Vec<U>) {
+    println!{"@[NETWORK]>> Sent {:?}.", &msg_buffer};
+    network.append(msg_buffer);
+    println!{"@[NETWORK]>> Message history {:?}.", network};
 }
